8f0cf977e5134592a004b8897de60c02ed248629
Angel Jimenez <angeljpp29@gmail.com>
2018-10-10 00:01:30 -0600

    Abrir el Cuadro de dialogo para seleccionar el archivo

--- app/src/main/java/com/example/angel/laboratorio2_ed2/MainActivity.java
+++ app/src/main/java/com/example/angel/laboratorio2_ed2/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.angel.laboratorio2_ed2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import Cifrado.CifradoZigZag;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button CifrarZigZag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CifrarZigZag=(Button) findViewById(R.id.CifrarZigZag);
+
+
     }
 }
