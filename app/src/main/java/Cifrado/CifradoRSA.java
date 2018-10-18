6781a726be03609b689f18fe2745916b32ac39c9
Angel Jimenez <angeljpp29@gmail.com>
2018-10-18 08:19:00 -0600

    Creacion de la clase RSA

---
+++ app/src/main/java/Cifrado/CifradoRSA.java
@@ -0,0 +1,16 @@
+package Cifrado;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.angel.laboratorio2_ed2.R;
+
+public class CifradoRSA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cifradorsa);
+        
+    }
+}
